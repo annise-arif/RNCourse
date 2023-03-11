d56d5ecc5e71ad6fee71068d3ca2f904009734af
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2023-03-11 12:28:27 +0600

    added Goal input and goal items component

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from './components/GoalItem';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((prev) => [...prev, {text: enteredGoalText, id: Math.random().toString()},]);
+
+  }
+  function deleteGoalHandler (id) {
+    setCourseGoals((prev) => {
+      return prev.filter((goal) => goal.id !== id);
+    });
+  }
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
-      </View>
-      <View>
-        <Text>List of golas...</Text>
+       <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.goalContainer}>
+      <FlatList
+      data={courseGoals} 
+      renderItem={(itemData) => {
+        return <GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler} />;
+      }}
+      keyExtractor={(item, index) => {
+        return item.id;
+      }}
+      alwaysBounceVertical={false}
+      />
       </View>
     </View>
   );
@@ -16,18 +36,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
+  
+  goalContainer: {
+    flex: 5,
   }
+
+
+
+
+  
 });
